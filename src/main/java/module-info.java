d9f3b3ba15fb751f770eb4d30c28f4a5aa179ed8
SimulatedDeer <distracteddeer@gmail.com>
2023-09-07 19:16:33 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.bluesea.downloadmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.bluesea.downloadmanager to javafx.fxml;
+    exports com.bluesea.downloadmanager;
+}
